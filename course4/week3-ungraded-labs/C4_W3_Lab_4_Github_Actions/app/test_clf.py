3feb2958f852667eb0a160e9d32d0d4092e37db3
Nguyendai202 <forworkainguyendai@gmail.com>
2024-02-05 18:04:38 +0700

    final_test

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
@@ -1,5 +1,3 @@
-import pickle
-from main import clf
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 def test_pipeline_and_scaler():
@@ -13,17 +11,3 @@ def test_pipeline_and_scaler():
         # sklearn.preprocessing.StandardScaler
         firstStep = [v for v in clf.named_steps.values()][0]
         assert isinstance(firstStep, StandardScaler)
-def test_accuracy():
-
-    # Load test data
-    with open("data/test_data.pkl", "rb") as file:
-        test_data = pickle.load(file)
-
-    # Unpack the tuple
-    X_test, y_test = test_data
-
-    # Compute accuracy of classifier
-    acc = clf.score(X_test, y_test)
-
-    # Accuracy should be over 90%
-    assert acc > 0.9
